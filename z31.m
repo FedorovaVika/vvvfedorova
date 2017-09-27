b2cb6b6009381abfb4b63edab8d03850cf10de93
FedorovaVika <32073057+FedorovaVika@users.noreply.github.com>
2017-09-27 20:48:46 +0300

    Update z31.m

--- z31.m
+++ z31.m
@@ -1,9 +1,19 @@
 pec1 = load('pec1.dat'); 
 fs = 1000;
-fcg = pec1(:, 1);
- ecg = pec1(:, 2); 
-cps = pec1(:, 3);
-t = (0:length(fcg) - 1)/fs;
-subplot(3,1,1), plot(t, fcg), xlim([1 4]); 
-subplot(3,1,2), plot(t, ecg), xlim([1 4]); 
-subplot(3,1,3), plot(t, cps), xlim([1 4]); 
+FCG = pec1(:, 1);
+ECG = pec1(:, 2); 
+CPS = pec1(:, 3);
+t = (0:length(FCG) - 1)/fs;
+figure(31);
+subplot(3,1,1), plot(t, FCG), xlim([1 4]); 
+xlim([1 4]), 
+ylabel('FCG'); 
+title ('Signal(t)');
+subplot(3,1,2), plot(t, ECG), xlim([1 4]); 
+xlim([1 4]), 
+ylabel('ECG'); 
+title ('Signal(t)');
+subplot(3,1,3), plot(t, CPS), xlim([1 4]); 
+xlim([1 4]), 
+ylabel('CPS'); 
+title ('Signal(t)');
