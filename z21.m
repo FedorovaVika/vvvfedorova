9aa1f77a52f10b0c5e0c8fbdf47b985fdc523026
FedorovaVika <32073057+FedorovaVika@users.noreply.github.com>
2017-09-27 20:26:28 +0300

    Update z21.m

--- z21.m
+++ z21.m
@@ -1,7 +1,17 @@
-fs = 200 ; 
-T = 0:1/fs:2 ; 
-D = 0.25 ;
-n = 0 : 1000; 
-r = randn(size(n))*sqrt(D) ;    					% шум
-s =1.8*cos(20*pi*n*T)  ;        				% сигнал
-x = s+r;                     % сигнал+шум
+fs = 200; 
+T = 1/fs; 
+D = 0.25;
+n = 0:1000-1;
+r = randn(size(n))*sqrt(D);    
+s = 1.8*cos(20*pi*n*T);        
+x = s + r;                     
+figure(21)
+plot(x);
+title('Stochastic signal')
+xlabel('n'), ylabel('x(n)')
+fprintf('Mv(s) = %4.3g\n', mean(s))
+fprintf('D(s) = %4.3g\n', var(s))
+fprintf('Mv(r) = %4.3g\n', mean(r))
+fprintf('D(r) = %4.3g\n', var(r))
+fprintf('Mv(x) = %4.3g\n', mean(x))
+fprintf('D(x) = %4.3g\n', var(x))
